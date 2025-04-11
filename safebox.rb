861bf40e59fb366f6244ff3b046cb9616cbeb7bd
jim <jim@jimdeMacBook-Air.local>
2025-04-11 13:29:37 +0900

    更新SafeBox Formula到版本 1.0.5

--- safebox.rb
+++ safebox.rb
@@ -1,8 +1,8 @@
 class Safebox < Formula
   desc "加密文件夹为单个二进制文件的工具"
   homepage "https://github.com/griffin928/kangaroo-bin"
-  url "https://github.com/griffin928/kangaroo-bin/raw/main/releases/kangaroo-1.0.4.tar.gz"
-  sha256 "bbb84a5682f35bcb81da2f51a6c29f76aeefc9a21f8f4aa47716eaba64b82b5f"
+  url "https://github.com/griffin928/kangaroo-bin/raw/main/releases/kangaroo-1.0.5.tar.gz"
+  sha256 "a81782f14cea6dcdb18a755a36392abe5ab5676cbeef2ad39960184343c6290f"
   license "专有"
   
   def install
