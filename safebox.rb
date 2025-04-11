124104c9113f533bd01149c250e72f37eaa81301
jim <jim@jimdeMacBook-Air.local>
2025-04-11 13:45:00 +0900

    更新Usafebox Formula到版本 1.0.7

--- safebox.rb
+++ safebox.rb
@@ -1,8 +1,8 @@
-class Safebox < Formula
+class Usafebox < Formula
   desc "加密文件夹为单个二进制文件的工具"
   homepage "https://github.com/griffin928/kangaroo-bin"
-  url "https://github.com/griffin928/kangaroo-bin/raw/main/releases/kangaroo-1.0.5.tar.gz"
-  sha256 "a81782f14cea6dcdb18a755a36392abe5ab5676cbeef2ad39960184343c6290f"
+  url "https://github.com/griffin928/kangaroo-bin/raw/main/releases/kangaroo-1.0.7.tar.gz"
+  sha256 "c6a3dfbfcb4e3ece34431125e8060bbf99442bd8fe97fde7a28e1230bd810e6f"
   license "专有"
   
   def install
