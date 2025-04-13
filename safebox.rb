91fa5ba7f36b28ff041ea8edc7e53a882cdb2b63
jim <jim@example.com>
2025-04-13 18:46:50 +0900

    更新Safebox Formula到版本 1.0.33

--- safebox.rb
+++ safebox.rb
@@ -1,8 +1,8 @@
 class Safebox < Formula
   desc "加密文件夹为单个二进制文件的命令行工具"
   homepage "https://github.com/griffin928/kangaroo-bin"
-  url "https://github.com/griffin928/kangaroo-bin/raw/main/releases/kangaroo-1.0.24.tar.gz"
-  sha256 "0a06db13b512d2f279d413bac628c055e2b9aa89f50233ab49556f486bba7f1a"
+  url "https://github.com/griffin928/kangaroo-bin/raw/main/releases/kangaroo-1.0.33.tar.gz"
+  sha256 "566f4b4bc8452003823c85885d27066024df1ba46ae45b2689d88ab773893591"
   license "专有"
   
   def install
